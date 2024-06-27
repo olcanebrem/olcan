64e5148c3566d75c8c3de9199141ecc9bd0900fe
Olcan EBREM <olcanebrem@gmail.com>
2024-06-27 03:40:46 +0300

    it works!

--- api/app.js
+++ api/app.js
@@ -1,13 +1,10 @@
-fetch('https://api.olcanebrem.com/api/data')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Fetch error:', error);
-  });
+const express = require('express');
+const app = express();
+
+app.use((req,res, next) => {
+    res.status(200).json({
+      message: "it works!"
+    });
+
+});
+module.exports = app;
--- api/server.js
+++ api/server.js
@@ -1,22 +1,8 @@
 const express = require('express');
-const cors = require('cors');
-const app = express();
+const http = require('http');
+const app = require('./app');
 const port = process.env.PORT || 3000; // Ortam değişkeninden portu al, yoksa 3000 kullan
-
-app.use(cors()); // CORS sorunlarını önlemek için
-app.use(express.json()); // JSON formatında gelen istekleri işlemek için
-
-// Basit bir GET endpoint
-app.get('/api/data', (req, res) => {
-    const data = {
-        message: 'Hello, World!',
-        timestamp: new Date()
-    };
-    res.status(200).send
-    res.json(data);
-});
+const server = http.createServer(app);
 
 // Sunucuyu başlat
-app.listen(port, () => {
-    console.log(`API server running on port ${port}`);
-});
+server.listen(port);
